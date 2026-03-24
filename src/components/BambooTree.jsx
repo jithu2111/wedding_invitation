6e90dd9620774cb53f8408961cfef1c165ade43e
jithu2111 <pchanna1@student.gsu.edu>
2026-03-24 13:04:27 -0400

    removed redundant code, and added tab icon

--- src/components/BambooTree.jsx
+++
@@ -1,8 +0,0 @@
-export default function BambooTree(props) {
-  return (
-    <mesh castShadow {...props}>
-      <cylinderGeometry args={[0.15, 0.15, 5, 8]} />
-      <meshStandardMaterial color="#2d4c1e" />
-    </mesh>
-  );
-}
